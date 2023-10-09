702b85f1e95362974bd2642f72c1364a3dbbd7de
Lam Nguyen <tunglam030699@gmail.com>
2023-10-08 21:34:07 -0400

    initial commit

--- server/index.js
+++ server/index.js
@@ -54,11 +54,11 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ONLY ADD DATA ONE TIME */
-    //Product.insertMany(dataProduct);
-    //ProductStat.insertMany(dataProductStat);
-    //User.insertMany(dataUser);
-    //Transaction.insertMany(dataTransaction);
-    //AffiliateStat.insertMany(dataAffiliateStat);
-    //OverallStat.insertMany(dataOverallStat);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
+    // User.insertMany(dataUser);
+    // Transaction.insertMany(dataTransaction);
+    // AffiliateStat.insertMany(dataAffiliateStat);
+    // OverallStat.insertMany(dataOverallStat);
   })
   .catch((error) => console.log(`${error} did not connect`));
